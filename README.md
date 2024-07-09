7d1e6a32a2d194299f65efe5f978e4d3b9a4a0f3
Jonathan Charles <jcharles8246@gmail.com>
2024-07-09 11:04:51 +1000

    Updated README, metrics generated with lookback intelligence, added confirm prompt for api calling to allow revision of competitions

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# wom-competition-generator
-Wise Old Man Competition Generator
+# Wise Old Man Competition Generator
+
+A small script that lets you generate multiple weekly Skill of the Week events in Wise Old Man.
+
+## Requirements
+
+* Python 3+ (Tested on Python 3.8.10)
+* A Wise Old Man "Group" and the "Group Verification Code" matching
+
+## Getting Started
+
+The script comes with help text, but below is an example execution of the tool
+
+```
+./generate_competitions.py --startDate 20240709 --numComps 100 --groupId 0 --groupVerificationCode 100-100-100
+```
+
+You can dry run the script to see what it _would_ generate
+
+```
+./generate_competitions.py --startDate 20240709 --numComps 100 --groupId 0 --groupVerificationCode 100-100-100 --dryRun
+```
--- generate_competitions.py
+++ generate_competitions.py
@@ -9,23 +9,7 @@ def add_args(parser):
     parser.add_argument('--groupVerificationCode', required=True, help='Group Verification Code of Wise Old Man Group')
     parser.add_argument('--dryRun', required=False, action='store_true', help='Dry run script')
 
-def main():
-    parser = argparse.ArgumentParser(description='Generate SOTW competitions on Wise Old Man for a group')
-    add_args(parser)
-    args = parser.parse_args()
-
-    # Inputs
-    numComps = args.numComps
-    groupId = args.groupId
-    groupVerificationCode = args.groupVerificationCode
-    startDateInput = args.startDate
-    dryRun = args.dryRun
-
-    start_date = datetime.datetime(int(startDateInput[0:4]), int(startDateInput[4:6]), int(startDateInput[6:8]), 18)
-    end_date = start_date + datetime.timedelta(days=6)
-
-    url = "https://api.wiseoldman.net/v2/competitions"
-    title = "Skill of the week"
+def get_random_metric(recent_args):
     metrics = ["attack",
             "strength",
             "defence",
@@ -50,31 +34,61 @@ def main():
             "hunter",
             "construction"
     ]
-    random.shuffle(metrics)
-   
+    return random.choice([item for item in metrics if item not in recent_args[-5:]])
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate SOTW competitions on Wise Old Man for a group')
+    add_args(parser)
+    args = parser.parse_args()
+
+    # Inputs
+    numComps = args.numComps
+    groupId = args.groupId
+    groupVerificationCode = args.groupVerificationCode
+    startDateInput = args.startDate
+    dryRun = args.dryRun
+
+    start_date = datetime.datetime(int(startDateInput[0:4]), int(startDateInput[4:6]), int(startDateInput[6:8]), 18)
+    end_date = start_date + datetime.timedelta(days=6)
+    url = "https://api.wiseoldman.net/v2/competitions"
+    title = "Skill of the week"
+    generated_metrics = []
+    payloads = []
+    
     # Generate POST payload and submit to generate competition on repeat
     for i in range(numComps): 
-        if i % 23 == 0:
-            random.shuffle(metrics)
         start_date_str = start_date.isoformat(timespec='milliseconds') + "Z"
         end_date_str = end_date.isoformat(timespec='milliseconds') + "Z"
+        metric = get_random_metric(generated_metrics)
         payload = {
             "title": title,
-            "metric": random.choice(metrics),
+            "metric": metric,
             "startsAt": start_date_str,
             "endsAt": end_date_str,
             "groupId": groupId,
             "groupVerificationCode": groupVerificationCode
         }
-        print(json.dumps(payload, indent=4))
-        print()
-        if not dryRun:
-            x = requests.post(url, json = payload)
-            print(x.text)
-        print("~~~~~~~~~~~~~~~~~~~~")
 
+        print("Week " + str(i) + ": " + metric)
+        payloads.append(payload)
+
+        generated_metrics.append(metric)
         start_date = start_date + datetime.timedelta(days=7)
         end_date = end_date + datetime.timedelta(days=7)
+
+    if not dryRun:
+        while (True):
+            answer = input("Would you like to generate these competitions? [y/n]")
+            if answer.lower() in ["y", "yes"]:
+                for payload in payloads:
+                    x = requests.post(url, json = payload)
+                    print(x.text)  
+                break
+            elif answer.lower() in ["n", "no"]:
+                print("Exiting...")
+                break
+            else:
+                print("Invalid input...try again")
 
 if __name__ == '__main__':
     main()
